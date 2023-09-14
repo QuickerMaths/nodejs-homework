2332d0a1dfa35a40590fdc30a18b54da5dba27d5
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-09-14 14:25:21 +0200

    created custom BaseError class and implemented it in global error Handler, refactored server responses

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import logger from "./middlewares/logger.js";
 import config from "./config/config.js";
 import db from "../db/db.js";
+import { BaseError } from "./utils/errors/BaseEror.js";
 
 // import contactsRouter from "./routes/api/contacts.js";
 
@@ -15,15 +16,31 @@ app.use(express.json());
 // app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  return res.status(404).json({
+    status: "FAILED",
+    code: 404,
+    message: "Not found",
+  });
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  if (err instanceof BaseError && err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: "FAILED",
+      code: err.statusCode,
+      message: err.message,
+    });
+  } else {
+    res
+      .status(500)
+      .json({ status: "FAILED", code: 500, message: "Internal Server Error" });
+    process.exit(1);
+  }
 });
 
 db.once("open", () => {
   app.listen(config.port, () => {
+    console.log("Database connection successful");
     console.log(`Server running. Use our API on port: ${config.port}`);
   });
 });
