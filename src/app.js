8accdc4db9bf1274364f3b24106f410651a91912
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-09-14 12:17:05 +0200

    deleted server.js and moved app.js file into src dir

--- src/app.js
+++ src/app.js
@@ -22,4 +22,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
+app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000");
+});
+
 export default app;
